774a851fc605d38f187591453f7ab3f03e1a8d1b
Lovi Stark <54571399+lovi-bhatia@users.noreply.github.com>
2023-01-14 19:50:05 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<script src="<?php echo get_template_directory_uri(); ?>/vendor/jquery/jquery.min.js"></script>
+
+<script src="<?php echo get_template_directory_uri(); ?>/vendor/typed.js/typed.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/vendor/aos/aos.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/vendor/particle js/particles.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/vendor/particle js/app.js"></script>
+
+
+<script src="<?php echo get_template_directory_uri(); ?>/app.js"></script>
+<?php wp_footer(); ?>
+</body>
+
+</html>
